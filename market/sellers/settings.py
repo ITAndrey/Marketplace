467a470e0ffad624d33f1d69df0d879f83c2627a
Tupikov Andrey <dimadokks@gmail.com>
2024-03-01 19:03:39 +0500

    the first page with the products

--- market/sellers/settings.py
+++ market/sellers/settings.py
@@ -118,13 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-
-MEDIA_URL = '/media/' # Создали отдельную папку для кранения всех картинок
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
