c7a2c3c7b77d5ded92babde0520fd3d4c4361bab
Tupikov Andrey <dimadokks@gmail.com>
2024-03-29 17:08:31 +0500

    postgresql

--- market/market/settings.py
+++ market/market/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'marketplace',
+        'USER' : 'market_admin',
+        'PASSWORD' : '89562876',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
@@ -119,8 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-MEDIA_URL = '/media/' # Создали отдельную папку для кранения всех картинок
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/Media/' # Создали отдельную папку для кранения всех картинок
+MEDIA_ROOT = BASE_DIR / 'Media'
 
 
 STATIC_URL = 'static/'
